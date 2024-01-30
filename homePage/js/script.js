334e35327f82d1896456e328253e5832b9581569
leandroVR1 <yeifry.121@gmail.com>
2024-01-30 06:39:49 -0500

    Modifiqué header, main y footer

--- homePage/js/script.js
+++ homePage/js/script.js
@@ -8,3 +8,21 @@ document.addEventListener("DOMContentLoaded", function () {
 		document.querySelector(".preloader").style.display = "none";
 	}, 1000);
 });
+
+const cboxCuidador = document.querySelector("#cboxCuidador");
+const cboxCliente = document.querySelector("#cboxCliente");
+
+cboxCuidador.addEventListener("change", function () {
+	if (cboxCuidador.checked) {
+		cboxCliente.disabled = true;
+	} else {
+		cboxCliente.disabled = false;
+	}
+});
+cboxCliente.addEventListener("change", function () {
+	if (cboxCliente.checked) {
+		cboxCuidador.disabled = true;
+	} else {
+		cboxCuidador.disabled = false;
+	}
+});
