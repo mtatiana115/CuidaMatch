0aefc40bd67d34f7eb1cef9220c66303cae5d025
SantiagoProgramador <andresx1277@gmail.com>
2024-02-02 09:40:39 -0500

    Guardian y script cambios en log in

--- homePage/js/guardian.js
+++ homePage/js/guardian.js
@@ -1,19 +1,20 @@
 (() => {
-    const isAuthenticated = localStorage.getItem('isAuthenticated');
-    const path = window.location.pathname;
-    const routeActu = path.substring(path.lastIndexOf('/') + 1);
+  const isAuthenticated = localStorage.getItem("isAuthenticated");
+  const path = window.location.pathname;
+  const routeActu = path.substring(path.lastIndexOf("/") + 1);
 
-    const privateRoutes = [
-        "main.html"
-    ];
+  const privateRoutes = ["main.html"];
+
+        
+
+  if (privateRoutes.includes(routeActu) && !isAuthenticated) {
+    console.log("No tienes permisos");
+    window.location.href = "index.html";
     
-    if (privateRoutes.includes(routeActu) && !isAuthenticated) {
-        window.location.href = "index.html";
-    }
-}
-)()
+  }
+})();
 
 function cerrarSesion() {
-    localStorage.removeItem("isAuthenticated");
-    window.location.href = "index.html";
+  localStorage.removeItem("isAuthenticated");
+  window.location.href = "index.html";
 }
--- homePage/js/script.js
+++ homePage/js/script.js
@@ -8,125 +8,134 @@ const cboxCuidadorlog = document.querySelector("#cboxCuidadorlogin");
 const cboxClientelog = document.querySelector("#cboxClientelogin");
 const ir_Regristro = document.querySelector("#goRegistro");
 const login = document.querySelector("#from-login");
+const userEmail = document.getElementById("exampleInputEmail1");
+const userPassword = document.getElementById("exampleInputPassword1");
 const URLcuidador = "http://localhost:3001/cuidadores";
 const URLbeneficiarios = "http://localhost:3001/beneficiarios";
 
 //preloader + eventos
 document.addEventListener("DOMContentLoaded", function () {
-	setTimeout(function () {
-		document.querySelector(".preloader").style.display = "none";
-	}, 1000);
+  setTimeout(function () {
+    document.querySelector(".preloader").style.display = "none";
+  }, 1000);
 });
 
 cboxCuidadorlog.addEventListener("change", function () {
-	if (cboxCuidadorlog.checked) {
-		cboxClientelog.checked = false;
-	}
+  if (cboxCuidadorlog.checked) {
+    cboxClientelog.checked = false;
+  }
 });
 cboxClientelog.addEventListener("change", function () {
-	if (cboxClientelog.checked) {
-		cboxCuidadorlog.checked = false;
-	} 
+  if (cboxClientelog.checked) {
+    cboxCuidadorlog.checked = false;
+  }
 });
 
 cboxCuidador.addEventListener("change", function () {
-	if (cboxCuidador.checked) {
-		cboxCliente.checked = false;
-	}
+  if (cboxCuidador.checked) {
+    cboxCliente.checked = false;
+  }
 });
 cboxCliente.addEventListener("change", function () {
-	if (cboxCliente.checked) {
-		cboxCuidador.checked = false;
-	} 
+  if (cboxCliente.checked) {
+    cboxCuidador.checked = false;
+  }
 });
 
 ir_Regristro.addEventListener("click", function () {
-	if (cboxCliente.checked) {
-		window.location.href = "../formPage/carebeneficiaryForm.html";
-	}
-	else if (cboxCuidador.checked) {
-		window.location.href = "../formPage/caregiverForm.html";
-	}
-	else {
-		Swal.fire({
-			title: "Parece que no elegiste una opción",
-			text: "Inténtalo de nuevo",
-			icon: "question",
-			timer: 2000,
-			showConfirmButton: false
-		});
-	}
+  if (cboxCliente.checked) {
+    window.location.href = "../formPage/carebeneficiaryForm.html";
+  } else if (cboxCuidador.checked) {
+    window.location.href = "../formPage/caregiverForm.html";
+  } else {
+    Swal.fire({
+      title: "Parece que no elegiste una opción",
+      text: "Inténtalo de nuevo",
+      icon: "question",
+      timer: 2000,
+      showConfirmButton: false,
+    });
+  }
 });
 // termina eventos
 
 // cargar
 login.addEventListener("submit", function (event) {
-    event.preventDefault();
-	if (cboxCuidadorlog.checked) {
-		logCuidador();
-	} else if (cboxClientelog.checked) {
-		logBeneficiario();
-	} else{
-		Swal.fire({
-            title: "Parece que no elegiste una opción",
-            text: "Inténtalo de nuevo",
-            icon: "question",
-            timer: 2000,
-            showConfirmButton: false
-        });
-	}
-	
+  event.preventDefault();
+  if (cboxCuidadorlog.checked) {
+    logCuidador();
+  } else if (cboxClientelog.checked) {
+    logBeneficiario();
+  } else {
+    Swal.fire({
+      title: "Parece que no elegiste una opción",
+      text: "Inténtalo de nuevo",
+      icon: "question",
+      timer: 2000,
+      showConfirmButton: false,
+    });
+  }
 });
 
 async function logCuidador() {
-	
-	if (!(await validarCuidador())) {
-        showAlert("El email no se encuentra registrado.");
-        return;
-    }
-    console.log("Iniciando sesión...");
-    localStorage.setItem("isAuthenticated", true);
+  if (!(await validarCuidador())) {
+    showAlert("El email no se encuentra registrado.");
+    return;
+  }
+  console.log("Iniciando sesión...");
+  localStorage.setItem(`isAuthenticated`, "true");
 }
 
 async function logBeneficiario() {
-	
-	if (!(await validarBeneficiario())) {
-        showAlert("El email no se encuentra registrado.");
-        return;
-    }
-    console.log("Iniciando sesión...");
-    localStorage.setItem("isAuthenticated", true);
+  if (!(await validarBeneficiario())) {
+    showAlert("El email no se encuentra registrado.");
+    return;
+  }
+  console.log("Iniciando sesión...");
+  localStorage.setItem("isAuthenticated", "true");
 }
 
 async function validarCuidador() {
-	try {
-        const response = await fetch(`${URLcuidador}?email=${emailCg.value}&password=${passwordCg.value}`);
-        const data = await response.json();
-        return data.length;
-    } catch (error) {
-        return false;
-    }
+  try {
+    const response = await fetch(
+      `${URLcuidador}?emailCg=${userEmail.value}&passwordCg=${userPassword.value}`
+    );
+    const data = await response.json();
+    return data.length;
+  } catch (error) {
+    return false;
+  }
 }
 
 async function validarBeneficiario() {
-	try {
-        const response = await fetch(`${URLbeneficiarios}?email=${emailCb.value}&password=${passwordCb.value}`);
-        const data = await response.json();
-        return data.length;
-    } catch (error) {
-        return false;
-    }
+  try {
+    const response = await fetch(
+      `${URLbeneficiarios}?emailCb=${userEmail.value}&passwordCb=${userPassword.value}`
+    );
+    const data = await response.json();
+    return data.length;
+  } catch (error) {
+    return false;
+  }
 }
 
 function showAlert(msg) {
-    Swal.fire({
-        title: 'Error!',
-        text: msg,
-        icon: 'error',
-        confirmButtonText: 'Ir de nuevo',
-        timer: 2000,
-        toast: "true",
-        position: "bottom-right",
-        showConfirmButton: false
-    })
+  Swal.fire({
+    title: "Error!",
+    text: msg,
+    icon: "error",
+    confirmButtonText: "Ir de nuevo",
+    timer: 2000,
+    toast: "true",
+    position: "bottom-right",
+    showConfirmButton: false,
+  });
+}
+
+function  obtenerInfo(id) {
+  const autenticacion = localStorage.getItem("isAuthenticated");
+  if (autenticacion == "true") {
+    window.location.href = "main.html";
+  }
+  
 }
