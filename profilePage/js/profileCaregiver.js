6a5279c67b8272a6547896098a3dfff7c5369ea7
Valeria Henao <valehm248@gmail.com>
2024-02-03 09:52:16 -0500

    Se modificó la imagen del perfil

--- profilePage/js/profileCaregiver.js
+++ profilePage/js/profileCaregiver.js
@@ -10,18 +10,19 @@ const workScheduleCg = document.querySelector("#workScheduleCg");
 const aboutMeCg = document.querySelector("#aboutMeCg");
 const skillsCg = document.querySelector("#skillsCg");
 const experienceCg = document.querySelector("#experienceCg");
-const holi = document.getElementById("holi")
+const imgCaregiver = document.querySelector("#imgCaregiver")
+// const holi = document.getElementById("holi")
 
 //Selectores de Editar
 
-const nombreEditar = document.getElementById("nombre-editar");
-const edadEditar = document.getElementById("edad-editar");
-const generoEditar = document.getElementById("genero-editar");
-const ciudadEditar = document.getElementById("ciudad-editar");
-const horarioTrabajoEditar = document.getElementById("horarioTrabajo-editar");
-const interesesEditar = document.getElementById("intereses-editar");
-const experienciaEditar = document.getElementById("experiencia-editar");
-const formCambios = document.getElementById("cambios");
+// const nombreEditar = document.getElementById("nombre-editar");
+// const edadEditar = document.getElementById("edad-editar");
+// const generoEditar = document.getElementById("genero-editar");
+// const ciudadEditar = document.getElementById("ciudad-editar");
+// const horarioTrabajoEditar = document.getElementById("horarioTrabajo-editar");
+// const interesesEditar = document.getElementById("intereses-editar");
+// const experienciaEditar = document.getElementById("experiencia-editar");
+// const formCambios = document.getElementById("cambios");
 
 
 
@@ -83,7 +84,7 @@ function actualizarPerfil(data) {
 		console.log("Usuario no encontrado");
 	} else {
 		console.log("Actualizando:", data);
-
+		imgCaregiver.src = data.profilePicCg;
 		nameUserCg.textContent = data.nameCg;
 		ageDisplayCg.textContent = data.ageCg;
 		cityCg.textContent = data.cityCg;
@@ -95,61 +96,61 @@ function actualizarPerfil(data) {
 			skillsCg.innerHTML += `<li>${element}</li>`;
 		});
 		//profilepic
-		holi.setAttribute("dataid", `${data.idCg}`)
+		// holi.setAttribute("dataid", `${data.idCg}`)
 	}
 
 	
-	editar();
+	// editar();
 }
 
 
 
-function editar(){
+// function editar(){
 
-nombreEditar.value = nameUserCg.textContent;
-edadEditar.value = ageDisplayCg.textContent;
-generoEditar.value = genderCg.textContent;
-ciudadEditar.value = cityCg.textContent;
-horarioTrabajoEditar.value = workScheduleCg.textContent;
+// nombreEditar.value = nameUserCg.textContent;
+// edadEditar.value = ageDisplayCg.textContent;
+// generoEditar.value = genderCg.textContent;
+// ciudadEditar.value = cityCg.textContent;
+// horarioTrabajoEditar.value = workScheduleCg.textContent;
 
 
 
-formCambios.addEventListener("submit", (event)=>{
-	event.preventDefault();
-	document.getElementById("close").click();
-	actualizarBaseDatos();
-	getUser();
-});
-};
+// formCambios.addEventListener("submit", (event)=>{
+// 	event.preventDefault();
+// 	document.getElementById("close").click();
+// 	actualizarBaseDatos();
+// 	getUser();
+// });
+// };
 
-async function actualizarBaseDatos(){
-	const userIdCaregiver = localStorage.getItem("userIdCg");
-	await fetch (`${url}/${userIdCaregiver}`,{
-		method: "PUT",
-		headers: {
-			"Content-Type": "application/json"
-		},
-		body: JSON.stringify({
-	"id": "123456",
+// async function actualizarBaseDatos(){
+// 	const userIdCaregiver = localStorage.getItem("userIdCg");
+// 	await fetch (`${url}/${userIdCaregiver}`,{
+// 		method: "PUT",
+// 		headers: {
+// 			"Content-Type": "application/json"
+// 		},
+// 		body: JSON.stringify({
+// 	"idCg": "123456",
       
-      "nameCg": "Valeria Henao",
+//       "nameCg": "Valeria Henao",
       
-      "passwordCg": "12356",
+//       "passwordCg": "12356",
       
-      "idCg": "123456",
-      "ageCg": "28",
-      "cityCg": "Medellin",
-      "genderCg": "Femenino",
-      "scheduleCg": "Por horas",
-      "professionCg": "Psicologia",
-      "experienceCg": "toda la vida",
-      "skillsCg": [
-        "Acompañamiento a citas",
-        "Compañía constante",
-        "Actividades lúdicas o fisioterapia",
-        "Cuidados postoperatorios"
-      ]
+//       "idCg": "123456",
+//       "ageCg": "28",
+//       "cityCg": "Medellin",
+//       "genderCg": "Femenino",
+//       "scheduleCg": "Por horas",
+//       "professionCg": "Psicologia",
+//       "experienceCg": "toda la vida",
+//       "skillsCg": [
+//         "Acompañamiento a citas",
+//         "Compañía constante",
+//         "Actividades lúdicas o fisioterapia",
+//         "Cuidados postoperatorios"
+//       ]
 		
-	})
-});
-}
+// 	})
+// });
+// }
