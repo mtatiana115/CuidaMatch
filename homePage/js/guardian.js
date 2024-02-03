3dfcc20f7f3512c5a9f69ed7ed95764e18e04434
SantiagoProgramador <andresx1277@gmail.com>
2024-02-03 12:13:51 -0500

    arreglé error de guardian

--- homePage/js/guardian.js
+++ homePage/js/guardian.js
@@ -2,7 +2,7 @@ function guardianCb() {
   const isAuthenticatedCb = localStorage.getItem("userId");
   const path = window.location.pathname;
   const routeActu = path.substring(path.lastIndexOf("/") + 1);
-  const privateRoutes = ["mainCarebeneficiary.html","profileCarebeneficiary.html"];
+  const privateRoutes = ["mainCarebeneficiary.html"];
 
   if (privateRoutes.includes(routeActu) && isAuthenticatedCb == null) {
     alert("No tienes permisos");
@@ -15,7 +15,7 @@ function guardianCg() {
   const isAuthenticatedCg = localStorage.getItem("userIdCg");
   const path = window.location.pathname;
   const routeActu = path.substring(path.lastIndexOf("/") + 1);
-  const privateRoutes = ["mainCaregiver.html","profileCaregiver.html"];
+  const privateRoutes = ["mainCaregiver.html"];
   
   if (privateRoutes.includes(routeActu) && isAuthenticatedCg == null) {
     alert("No tienes permisos");
