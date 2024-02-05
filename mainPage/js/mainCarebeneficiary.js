e9668c8430599fb0b48bc68dda93fd9bc1167022
SantiagoProgramador <andresx1277@gmail.com>
2024-02-05 10:46:52 -0500

    main cambios

--- mainPage/js/mainCarebeneficiary.js
+++ mainPage/js/mainCarebeneficiary.js
@@ -3,19 +3,19 @@ const url = "http://localhost:3001/beneficiarios";
 
 //preloader
 document.addEventListener("DOMContentLoaded", function () {
-	setTimeout(function () {
-		document.querySelector(".preloader").style.display = "none";
-	}, 1000);
+  setTimeout(function () {
+    document.querySelector(".preloader").style.display = "none";
+  }, 1000);
 
-	printUsers();
+  printUsers();
 });
 
 async function printUsers() {
-	const response = await fetch(url);
-	const data = await response.json();
-	console.log(data);
-	data.forEach((user) => {
-		main.innerHTML += `
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  data.forEach((user) => {
+    main.innerHTML += `
 		<div class="card">
 				<div class="wrapper">
 					<div class="profile">
@@ -65,23 +65,26 @@ async function printUsers() {
 							No me gusta
 						</button>
 						<button class="like" data-id="${user.id}" onclick="likeAction(this)">Me gusta</button>
+						<button type="button" class="findProfile" data-id="${user.id}" onclick="findProfile(this)" data-toggle="modal" data-target="#infoProfile">
+							Ver perfil
+						</button>
 					</div>
 				</div>
 			</div>
 		`;
-	});
+  });
 }
 
 // Cards Animations
 async function likeAction(btn) {
-	const chat = document.querySelector(".chat");
-	const idCb = btn.getAttribute("data-id");
+  const chat = document.querySelector(".chat");
+  const idCb = btn.getAttribute("data-id");
 
-	const respuesta = await fetch(`${url}/${idCb}`);
-	const data = await respuesta.json();
-	console.log(data);
+  const respuesta = await fetch(`${url}/${idCb}`);
+  const data = await respuesta.json();
+  console.log(data);
 
-	chat.innerHTML += `
+  chat.innerHTML += `
 	<li class="message">
 		<img
 			class="logo"
@@ -92,17 +95,65 @@ async function likeAction(btn) {
 	</li>
 	`;
 
-	const card = btn.closest(".card");
-	card.classList.add("like-animation");
-	setTimeout(() => {
-		card.remove();
-	}, 1000);
+  const card = btn.closest(".card");
+  card.classList.add("like-animation");
+  setTimeout(() => {
+    card.remove();
+  }, 1000);
 }
 
 function dislikeAction(button) {
-	const card = button.closest(".card");
-	card.classList.add("dislike-animation");
-	setTimeout(() => {
-		card.remove();
-	}, 1000);
+  const card = button.closest(".card");
+  card.classList.add("dislike-animation");
+  setTimeout(() => {
+    card.remove();
+  }, 1000);
+}
+
+async function findProfile(button) {
+  const contenedorInfo = document.querySelector("#FindprofileContainer");
+
+  const idCb = button.getAttribute("data-id");
+  const respuesta = await fetch(`${url}/${idCb}`);
+  const data = await respuesta.json();
+
+  contenedorInfo.innerHTML = `
+		<div
+      class="modal fade"
+      id="infoProfile"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="infoProfile"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">...</div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+	`;
+  console.log(data);
+  console.log(contenedorInfo);
 }
